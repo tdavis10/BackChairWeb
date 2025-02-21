593b29c38138f4dd4c5e4ddb9c466dbee9e9fee5
Tanner Davis <9918654-TannerDavis3@users.noreply.replit.com>
2025-02-21 22:13:17 +0000

    Restored to '37fa6fd68bded5231fbdf5299fdced77c9629aa1'
    Replit-Restored-To:37fa6fd68bded5231fbdf5299fdced77c9629aa1

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,5 @@
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Switch, Route } from "wouter";
-import { Header } from "@/components/header";
 import { queryClient } from "./lib/queryClient";
 import { Toaster } from "@/components/ui/toaster";
 import NotFound from "@/pages/not-found";
@@ -29,7 +28,6 @@ function App() {
     <QueryClientProvider client={queryClient}>
       <AuthProvider>
         <div className="flex min-h-screen flex-col">
-          <Header />
           <Router />
           <Footer />
         </div>
